7aaf92dedd5f9fdf29e8ad9bf99f642d8191dd35
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-15 17:40:07 +0330

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,22 @@ function giveScore() {
 
 function guess() {
   guessNumber = document.querySelector(".guess-number").value;
+  console.log(guessNumber);
 
-  if (+guessNumber === secretNumber) {
+  if (!+guessNumber || +guessNumber > 20 || +guessNumber < 1) {
+    document.querySelector(".anouncement").textContent =
+      "😉You must choose number in range!!!";
+  } else if (+guessNumber === secretNumber) {
     document.querySelector(".anouncement").textContent = "🥳You win the game";
     document.querySelector(".secret-number").textContent = secretNumber;
+    document.querySelector(".secret-number").style.width = "200px";
+    document.querySelector(".secret-number").style.height = "200px";
     document.querySelector("body").style.backgroundColor = "rgb(56, 217, 31)";
+    document.querySelector(".highest-score").textContent = scoring;
   } else if (+guessNumber > secretNumber) {
     document.querySelector(".anouncement").textContent = "📈too high!";
     giveScore();
-  } else {
+  } else if (+guessNumber < secretNumber) {
     document.querySelector(".anouncement").textContent = "📉too low!";
     giveScore();
   }
