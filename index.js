d22f6813eb7ed78b2ed1d57cd0865cee5e9839f3
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-15 18:09:59 +0330

    added function to button-"again"

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let scoring = 20;
-
+let highestScoring = 0;
 function giveScore() {
   scoring--;
   document.querySelector(".score").textContent = scoring;
-  if (scoring >= 1) {
-    document.querySelector(".score").textContent = scoring;
-  } else {
+  if (scoring < 1) {
     document.querySelector(".anouncement").textContent = "😬You lost the game!";
   }
 }
 
 function guess() {
   guessNumber = document.querySelector(".guess-number").value;
-  console.log(guessNumber);
 
   if (!+guessNumber || +guessNumber > 20 || +guessNumber < 1) {
     document.querySelector(".anouncement").textContent =
@@ -24,7 +21,10 @@ function guess() {
     document.querySelector(".secret-number").style.width = "200px";
     document.querySelector(".secret-number").style.height = "200px";
     document.querySelector("body").style.backgroundColor = "rgb(56, 217, 31)";
-    document.querySelector(".highest-score").textContent = scoring;
+    if (highestScoring < scoring) {
+      highestScoring = scoring;
+    }
+    document.querySelector(".highest-score").textContent = highestScoring;
   } else if (+guessNumber > secretNumber) {
     document.querySelector(".anouncement").textContent = "📈too high!";
     giveScore();
@@ -34,4 +34,16 @@ function guess() {
   }
 }
 
+function repeatGame() {
+  document.querySelector(".anouncement").textContent = "🫣 Starting to guess...";
+  document.querySelector(".secret-number").textContent = "?";
+  document.querySelector(".secret-number").style.width = "100px";
+  document.querySelector(".secret-number").style.height = "100px";
+  document.querySelector("body").style.backgroundColor = "rgb(16, 16, 16)";
+  document.querySelector(".guess-number").value = "";
+  document.querySelector(".score").textContent = 20;
+  scoring = 20;
+}
+
 document.querySelector(".guess").addEventListener("click", guess);
+document.querySelector(".again").addEventListener("click", repeatGame);
